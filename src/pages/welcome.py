1b80a2771803b6196f63085ebcd12b39c49722eb
Isabelle Jackson <izzyjackson05@outlook.com>
2026-05-30 18:39:20 +0100

    feat: restructure code

--- src/pages/welcome.py
+++ src/pages/welcome.py
@@ -1,11 +1,40 @@
 from gi.repository import Adw, Gtk
 
+from gettext import gettext as _
+
 @Gtk.Template(resource_path='/dev/getapollo/Eleven/welcome.ui')
 class ElevenWelcomePage(Adw.Bin):
     __gtype_name__ = 'ElevenWelcomePage'
 
     status_page = Gtk.Template.Child()
+    
     install_btn = Gtk.Template.Child()
+    try_btn = Gtk.Template.Child()
 
-    def __init__(self, **kwargs):
+    service = None
+
+    def __init__(self, service, **kwargs):
         super().__init__(**kwargs)
+
+        self.service = service
+
+        # Translators: os_name is the name of the operating system being installed
+        self.status_page.set_title(
+            _("Welcome to {os_name}").format(
+                os_name=self.service.os_name
+            )
+        )
+
+        # Translators: os_name is the name of the operating system being installed
+        self.status_page.set_description(
+            _("You can try {os_name} out in the live environment. When you're ready to install, you can use this program to install {os_name} to this device").format(
+                os_name=self.service.os_name
+            )
+        )
+        
+        # Translators: os_name is the name of the operating system being installed
+        self.install_btn.set_label(
+            _("Install {os_name}").format(
+                os_name=self.service.os_name
+            )
+        )
--- src/window.py
+++ src/window.py
@@ -10,33 +10,17 @@ class ElevenWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'ElevenWindow'
 
     welcome_view = Gtk.Template.Child()
-    welcome_page = Gtk.Template.Child()
 
     def __init__(self, service, **kwargs):
         super().__init__(**kwargs)
 
         self.service = service
 
-        # Translators: os_name is the name of the operating system being installed
-        self.welcome_title = _("Welcome to {os_name}").format(
-            os_name=self.service.os_name
-        )
-        
-        # Translators: os_name is the name of the operating system being installed
-        self.welcome_message = _(
-            "You can try {os_name} out in the live environment. When you're ready to install, you can use this program to install {os_name} to this device"
-        ).format(
-            os_name=self.service.os_name
-        )
+        self.welcome_page = ElevenWelcomePage(self.service)
 
-        self.welcome_page.status_page.set_title(self.welcome_title)
-        self.welcome_page.status_page.set_description(self.welcome_message)
-        
-        # Translators: os_name is the name of the operating system being installed
-        self.welcome_page.install_btn.set_label(
-            _("Install {os_name}").format(
-                os_name=self.service.os_name
-            )
-        )
+        self.welcome_view.set_child(self.welcome_page)
 
-        self.welcome_view.set_title(self.welcome_title)
+        self.welcome_page.try_btn.connect("clicked", self.try_os)
+    
+    def try_os(self, _button):
+        self.close()
