60b1a6d5badee0831bc2b92b0adafaeeaa31f1ba
Izzy <izzyjackson05@outlook.com>
2026-08-01 15:00:47 +0100

    feat: better log messages

--- src/service/service.py
+++ src/service/service.py
@@ -9,9 +9,12 @@ class ElevenInstallerService():
     disks = ElevenInstallerServicePartitioner(log=log)
 
     def __init__(self, *kwargs):
-        self.log.new_entry(1, "Installation is starting now", 0)
+        self.log.new_entry(1, "Starting installer service", 0)
 
         if self.config.check_config_exists():
             self.config.load_config()
+
+        if self.config.exists and self.config.valid:
+            self.log.new_entry(1, "Configuration exists and is valid. Starting installer", 0)
 
         self.disks.list_all_drives()
